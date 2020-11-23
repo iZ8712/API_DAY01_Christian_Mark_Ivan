22b79064e295a250f5a3116e61ac842f7adab0f4
Christian00000 <c.froemmer@gmail.com>
2020-11-23 15:36:33 +0100

    minor style changes

--- Ex01/index.php
+++ Ex01/index.php
@@ -8,44 +8,69 @@
 
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Marc - Ivan - Christians NewsFeed</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+     </ul>
+  </div>
+</nav>
 <div class="container-fluid">
-<div class="row justify-content-around">
-
-
-<?php 
-
+<div class="row ">
+<div class="col-12" >
+<?php
 require_once 'RESTful.php';
+
+
+$url = 'http://api.serri.codefactory.live/random/';
+$response = curl_get($url);
+$json = json_decode($response);
+$joke = $json->joke;
+$id = $json->id_joke; 
+
+
+ 
+//var_dump($json->joke);
+echo '<div class="jumbotron">
+<h1 class="display-4">Joke of the Day!</h1>
+<p class="lead">Presented by Serri\'s joke No.'.$id.'</p>
+
+<p>'.$joke.'</p>
+<a class="btn btn-primary btn-lg" href="index.php" role="button">Next joke</a>
+</div>';
+
+
+?>
+
+</div>
+
+<div class="row justify-content-center ">
+<?php
 
 $url = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
 $response = curl_get($url);
 $xml = simplexml_load_string($response);
 foreach ($xml->channel->item as $item) {
-    echo '<div class="card" style="width: 18rem;"><div class="card-body">
+    echo '<div class="card mt-2 mb-2 col-4" "><div class="card-body ">
     <h5 class="card-title"><a href="'.$item->link.'" target"_blank">'.$item->title.'</a></h5>
     <p class="card-text">'.$item->description.'</p></div></div>';
 }
 ?>
-
+</div>
 </div>
 <div class="row justify-content-around">
 
-<?php
-$url = 'http://api.serri.codefactory.live/random/';
-$response = curl_get($url);
-$json = json_decode($response);
-$joke = $json->joke;
-$id = $json->id_joke;
-
-//var_dump($json->joke);
-echo '<div class="card" style="width: 18rem;"><div class="card-body">
-<h5 class="card-title">Serri\'s joke No.'.$id.'</h5>
-<p class="card-text">'.$joke.'</p></div></div>';
-
-?>
-
 
 </div>
+
+
 </div>
 
 
